a0f58af3f83bbf091b81bdba035de8ec62687ac1
stas.budzyn <stas.budzyn@gmail.com>
2023-01-31 23:43:45 +0100

    Implemented TokenService

---
+++ HelpMeApp.Server/HelpMeApp.Services/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using HelpMeApp.DatabaseAccess.Entities.AppUserEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpMeApp.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateToken(AppUser user);
+    }
+}
