bb42f969554a43df829f5ab39a639ad60bc89cef
stas.budzyn <stas.budzyn@gmail.com>
2023-02-02 17:31:33 +0100

    Added logic of retrieving adverts in repositories

---
+++ HelpMeApp.Server/HelpMeApp.DatabaseAccess/Interfaces/IAdvertRepository.cs
@@ -0,0 +1,15 @@
+﻿using HelpMeApp.DatabaseAccess.Entities.AdvertEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpMeApp.DatabaseAccess.Interfaces
+{
+    public interface IAdvertRepository
+    {
+        Task<IEnumerable<Advert>> GetAdvertsByPage(int page, int pageSize);
+        Task<Advert> GetAdvertById(int id);
+    }
+}
---
+++ HelpMeApp.Server/HelpMeApp.DatabaseAccess/Repositories/AdvertRepository.cs
@@ -0,0 +1,37 @@
+﻿using HelpMeApp.DatabaseAccess.Entities.AdvertEntity;
+using HelpMeApp.DatabaseAccess.Interfaces;
+using HelpMeApp.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpMeApp.DatabaseAccess.Repositories
+{
+    public class AdvertRepository : IAdvertRepository
+    {
+        private HelpMeDbContext _context;
+        public AdvertRepository(HelpMeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Advert>> GetAdvertsByPage(int page, int pageSize)
+        {
+            return await _context.Adverts
+                .Where(a => a.IsClosed == false)
+                .OrderBy(a => a.Id)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task<Advert> GetAdvertById(int id)
+        {
+            return await _context.Adverts.FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+    }
+}
