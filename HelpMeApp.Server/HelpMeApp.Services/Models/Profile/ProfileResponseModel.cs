2b0562187ac4179bdcfe43c2cc753f4b13626576
Oleksandr Petrov <petrovoleksandr03@gmail.com>
2023-02-07 13:34:51 +0100

    Comments resolved 1

---
+++ HelpMeApp.Server/HelpMeApp.Services/Models/Profile/ProfileResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpMeApp.Services.Models.Profile
+{
+    public class ProfileResponseModel<T>
+    {
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ HelpMeApp.Server/HelpMeApp.WebAPI/Controllers/ProfileController.cs
@@ -0,0 +1,36 @@
+﻿using HelpMeApp.DatabaseAccess.Entities.AppUserEntity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using HelpMeApp.Services.Models.Profile;
+using HelpMeApp.DatabaseAccess.Entities.AdvertEntity;
+using System.Collections.Generic;
+using HelpMeApp.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using HelpMeApp.Services.Services.Profile;
+
+namespace HelpMeApp.WebAPI.Controllers.Profile
+{
+    [Route("api/profile")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private UserManager<AppUser> _userManager;
+        private IProfileService _profileService;
+
+        public ProfileController(UserManager<AppUser> userManager, IProfileService profileService)
+        {
+            this._userManager = userManager;
+            this._profileService = profileService;
+        }
+
+        [HttpGet("GetUserById/{id}")]
+        public async Task<ActionResult<ProfileResponseModel<ProfileRequestModel>>> GetUserById(string id)
+        {
+            var result = await _profileService.GetUserById(id);
+            return result;
+
+        }
+    }
+}
