44d69cd23232f9b555682fbce790bf463776b4ff
Oleksandr Petrov <petrovoleksandr03@gmail.com>
2023-02-09 20:54:30 +0100

    Username removed from requestModel

--- HelpMeApp.Server/HelpMeApp.Services/Models/Profile/ProfileResponseData.cs
+++ HelpMeApp.Server/HelpMeApp.Services/Models/Profile/ProfileResponseData.cs
@@ -9,7 +9,6 @@ namespace HelpMeApp.Services.Models.Profile
 {
     public class ProfileResponseData
     {
-        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
